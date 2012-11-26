7c98b611e31a51e15c04a2f680a295cc55371d23
rma4ok <rma4ok@gmail.com>
2012-11-26 16:33:30 -0500

    [refactor] single var

--- src/2.Helper.js
+++ src/2.Helper.js
@@ -1,29 +1,38 @@
 var Helper = {
-  extend          : function (target, source) {
+  extend: function (target, source) {
+    var key;
+
     if (source == null) return target;
     if (target == null) target = {};
-    for (var key in source) target[key] = source[key];
+    for (key in source)
+      target[key] = source[key];
     return target;
   },
-  getProperty     : function (o, chain) {
+
+  getProperty: function (o, chain) {
     if (typeof o !== 'object' || chain == null) return o;
     if (typeof chain === 'string') chain = chain.split('.');
     if (chain.length === 1) return o[chain[0]];
     return this.getProperty(o[chain.shift()], chain);
   },
-  templateFunction: function (arr, o) {
 
-    var output = '';
-    for (var i = 0; i < arr.length; i++) {
+  templateFunction: function (arr, o) {
+    var
+        output = '',
+        utility, value, index,
+        key, i, length;
+
+    for (i = 0, length = arr.length; i < length; i++) {
       if (i % 2 === 0) {
         output += arr[i];
-      } else {
-        var key = arr[i],
-            value = null,
-            index = key.indexOf(':');
+      }
+      else {
+        key = arr[i];
+        value = null;
+        index = key.indexOf(':');
 
         if (index > -1) {
-          var utility = index > 0 ? key.substring(0, index).replace(regexpWhitespace, '') : '';
+          utility = index > 0 ? key.substring(0, index).replace(regexpWhitespace, '') : '';
           if (utility === '') utility = 'condition';
 
           key = key.substring(index + 1);
