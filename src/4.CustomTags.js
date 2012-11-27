b941a19dbe70fa05f6b0ac58d7d65ad23b884351
rma4ok <rma4ok@gmail.com>
2012-11-26 19:36:02 -0500

    [feature] support binding for older browsers

--- src/4.CustomTags.js
+++ src/4.CustomTags.js
@@ -66,20 +66,114 @@ var CustomTags = function () {
     this.attr = {}
   }
 
-  Binding.prototype.render = function (values, container) {
-    var value = values[this.attr.value];
-    Object.defineProperty(values, this.attr.value, {
-      get: function () {
-        return value;
-      },
-      set: function (x) {
-        container.innerHTML = (value = x);
-      }
-    });
-    container.innerHTML = value;
-    return container;
-  };
+  Binding.prototype.render = function () {
 
+    // lazy self definition
+    return (Binding.prototype.render = function () {
+
+      var
+          ObjectDefineProperty = Object.defineProperty,
+          supportsDefineProperty = false,
+          watchedObjects;
+
+      if (ObjectDefineProperty) {
+        try {
+          supportsDefineProperty = Object.defineProperty({}, 'x', {get: function () {
+            return true
+          }}).x;
+        }
+        catch (e) {
+          supportsDefineProperty = false;
+        }
+      }
+      else {
+        if (Object.prototype.__defineGetter__) {
+          ObjectDefineProperty = function (obj, prop, desc) {
+            if (hasOwnProperty.call(desc, 'get')) obj.__defineGetter__(prop, desc.get);
+            if (hasOwnProperty.call(desc, 'set')) obj.__defineSetter__(prop, desc.set);
+          };
+
+          supportsDefineProperty = true;
+        }
+      }
+
+      if (!supportsDefineProperty) {
+        watchedObjects = [];
+
+        ObjectDefineProperty = function (obj, prop, desc) {
+          var
+              objectWrapper,
+              found = false,
+              i, length;
+
+          for (i = 0, length = watchedObjects.length; i < length; i++) {
+            objectWrapper = watchedObjects[i];
+            if (objectWrapper.obj === obj) {
+              found = true;
+              break;
+            }
+          }
+
+          if (!found)
+            objectWrapper = watchedObjects[i] = {obj: obj, props: {}};
+
+          objectWrapper.props[prop] = {
+            value: obj[prop],
+            set  : desc.set
+          };
+        };
+
+        function ticker() {
+          var
+              objectWrapper,
+              i, length,
+              props,
+              prop,
+              propObj,
+              newValue;
+
+          for (i = 0, length = watchedObjects.length; i < length; i++) {
+            objectWrapper = watchedObjects[i];
+            props = objectWrapper.props;
+
+            for (prop in props)
+              if (hasOwnProperty.call(props, prop)) {
+                propObj = props[prop];
+                newValue = objectWrapper.obj[prop];
+                if (newValue !== propObj.value)
+                  propObj.set.call(null, newValue);
+              }
+          }
+
+          setTimeout(ticker, 16);
+        }
+
+        ticker();
+      }
+
+
+      return function (values, container) {
+        var
+            attrValue = this.attr.value,
+            value = values[attrValue];
+
+        ObjectDefineProperty.call(Object, values, attrValue, {
+          get: function () {
+            return value;
+          },
+          set: function (x) {
+            container.innerHTML = value = x;
+          }
+        });
+
+        container.innerHTML = value;
+        return container;
+      }
+
+    }()
+
+        ).apply(this, arguments);
+  };
 
   return {
     all: {
