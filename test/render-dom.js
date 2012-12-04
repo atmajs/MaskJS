94d738db4d55881bc501cbc6d4b9c0f8d44d7db2
Alexander Tenbits <source.kit@gmail.com>
2012-12-04 10:56:58 +0100

    [++] condition test, model render test

--- test/render-dom.js
+++ test/render-dom.js
@@ -6,5 +6,21 @@ buster.testCase("Render", {
 		var dom = render('div > div.test');
 
 		assert(dom.querySelector('.test') != null);
+	},
+	'right model insertion': function() {
+		var dom = render('div##{id}.#{klass} data-type="#{type}" > "#{name}"', {
+			name: 'NAME',
+			id: 'ID',
+			klass: 'CLASS',
+			type: 'TYPE'
+		}).querySelector('div');
+		
+		assert(dom != null, 'DIV not redered');
+		
+		assert(dom.getAttribute('id') == 'ID', 'id is not ID');
+		assert(dom.getAttribute('class') == 'CLASS', 'class is not CLASS');
+		assert(dom.getAttribute('data-type') == 'TYPE', 'data-type is not TYPE');
+		
+		assert(dom.innerText == 'NAME', 'text is not NAME');
 	}
 })
--- test/valueutility-node.js
+++ test/valueutility-node.js
@@ -1,6 +1,7 @@
 var buster = require('buster'),
 	mask = require('../lib/mask.node.js'),
 	isCondition = mask.ValueUtils.out.isCondition,
+	condition = mask.ValueUtils.condition,
 	template, node, attr;
 
 buster.testCase("Value Utils", {
@@ -30,5 +31,30 @@ buster.testCase("Value Utils", {
 			falsy: false,
 			truthy: true
 		}));
+	},
+	'valid condition returns': function(){
+		assert(condition('enabled ? klass1 : klass2',{
+			klass1:'A',
+			klass2: 'B',
+			enabled: true
+		}) == 'A', 'Enabled, but not A');
+
+		assert(condition('klass1=="A" && !enabled ? klass1 : klass2',{
+			klass1:'A',
+			klass2: 'B',
+			enabled: false
+		}) == 'A', 'klass1 is A and disabled, but returns not A');
+
+		assert(condition('klass1=="A" && ("A"=="B" || "A"=="A") ? "isB" : "isA"',{}) == 'isA', 'Hardcoded A is not A');
+		assert(condition('klass1=="A" && ("A"=="B" || "A"=="C") ? "isB" : "isA"',{}) == 'isA', 'Falsy Hardcoded A, but is anyway A');
+
+		assert(condition('klass1==klass2 ? info.id : "not eq"',{
+			klass1:'A',
+			klass2: 'A',
+			enabled: false,
+			info: {
+				id: 'ID'
+			}
+		}) == 'ID', 'Are equal, but returns not ID');
 	}
 })
