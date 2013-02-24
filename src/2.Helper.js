4acc3252a60d72e375c36bd9abb499b7f86cde2b
rma4ok <rma4ok@gmail.com>
2013-02-24 15:54:20 -0500

    [refactor] Helper optimization

--- src/2.Helper.js
+++ src/2.Helper.js
@@ -2,12 +2,13 @@ var Helper = {
 	extend: function(target, source) {
 		var key;
 
-		if (source == null) {
-			return target;
-		}
-		if (target == null) {
+		//TODO: remove this fall through
+		// Whoever uses `extend` should pass an object
+		if (!target) {
 			target = {};
 		}
+
+		// if source == null for loop won't run
 		for (key in source) {
 			if (hasOwnProp.call(source, key)) {
 				target[key] = source[key];
@@ -18,21 +19,12 @@ var Helper = {
 
 	getProperty: function(o, chain) {
 		var value = o,
-			props, key, i, length;
+			props = chain.split('.'),
+			i = -1,
+			length = props.length;
 
-		if (typeof o !== 'object' || chain == null) {
-			return o;
-		}
-		if (typeof chain === 'string') {
-			props = chain.split('.');
-		}
-
-		for (i = 0, length = props.length; i < length; i++) {
-			key = props[i];
-			value = value[key];
-			if (!value) {
-				return value;
-			}
+		while (value && ++i < length) {
+			value = value[props[i]];
 		}
 
 		return value;
