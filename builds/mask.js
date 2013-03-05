73be532f3d46be0e005c065e086896f50a46a3cf
Alexander Kit <source.kit@gmail.com>
2013-03-05 14:49:24 +0100

    [refactor src]

--- builds/mask.js
+++ builds/mask.js
@@ -6,11 +6,11 @@
 // import ../src/1.scope-vars.js
 // import ../src/2.Helper.js
 // import ../src/3.Template.js
-// import ../src/5.ConditionUtil.js
+// import ../src/4.ConditionUtil.js
 // import ../src/5.Custom.js
 // import ../src/6.Parser.linked.js
 // import ../src/7.Builder.recursion.js
-// import ../src/8.Mask.js
+// import ../src/9.Mask.js
 
 
 // import ../src/8.CreateDocumentFragment.js
