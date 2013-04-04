8abb2a062ab13259ec5951a03b36504a36430262
Alexander Kit <source.kit@gmail.com>
2013-04-04 14:40:22 +0200

    [fix] binding: bind.util - arguments

--- src/handlers/mask.binding.js
+++ src/handlers/mask.binding.js
@@ -236,7 +236,7 @@ Bind.prototype.renderStart = function(model, cntx, container) {
  */
 
 
-mask.registerUtility('bind', function(property, model, cntx, element, controller, type, attrName){
+mask.registerUtility('bind', function(property, model, cntx, element, controller, attrName, type){
 	var current = getProperty(model, property);
 	switch(type){
 		case 'node':
@@ -606,55 +606,6 @@ function getValidations(component, out){
 		getValidations(x);
 	}
 	return out;
-}
-
-// source /src/bind.signals.js
-
-/**
- *	Mask Custom Attribute
- *	Bind Closest Controllers Handler Function to dom event(s)
- */
-
-mask.registerAttrHandler('x-signal', function(node, attrValue, model, cntx, element, controller){
-
-	var arr = attrValue.split(';');
-	for(var i = 0, x, length = arr.length; i < length; i++){
-		x = arr[i];
-		var event = x.substring(0, x.indexOf(':')),
-			handler = x.substring(x.indexOf(':') + 1).trim(),
-			Handler = getHandler(controller, handler);
-
-		if (Handler){
-			addEventListener(element, event, Handler);
-		}
-
-		// if DEBUG
-		!Handler && console.warn('No slot found for signal', handler, controller);
-		// endif
-	}
-
-});
-
-
-function getHandler(controller, name){
-	if (controller == null) {
-		return null;
-	}
-
-	if (controller.slots != null && typeof controller.slots[name] !== 'undefined'){
-		var slot = controller.slots[name];
-		if (typeof slot === 'string'){
-			slot = controller[slot];
-		}
-
-		// if DEBUG
-		typeof slot !== 'function' && console.error('Controller defines slot, but that is not a function', controller, name);
-		// endif
-
-		return slot.bind(controller);
-	}
-
-	return getHandler(controller.parent, name);
 }
 
 
