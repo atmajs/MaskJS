bba7b3930442a55ea24e29b785872bc35596e8b4
Alexander Kit <source.kit@gmail.com>
2013-04-04 22:17:59 +0200

    [dist] expression: extract var refs

--- src/expression/exports.js
+++ src/expression/exports.js
@@ -6,11 +6,13 @@ var ExpressionUtil = (function(){
 	// import 4.parser.helper.js
 	// import 5.parser.js
 	// import 6.eval.js
+	// import 7.vars.helper.js
 
 
 	return {
 		parse: expression_parse,
-		eval: expression_evaluate
+		eval: expression_evaluate,
+		varRefs: refs_extractVars
 	};
 
 }());
