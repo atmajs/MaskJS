0554bb9b0e837d9742f9a691edb9a4a621a2153b
Alexander Kit <source.kit@gmail.com>
2013-06-03 14:26:18 +0200

    [dist] use ijs test runner

---
+++ test/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	suites: {
+		'mask.lib': {
+			exec: 'node',
+			env: ['lib/mask.node.js::mask'],
+			tests: 'test/**-node.test'
+		},
+		
+		'mask.dom': {
+			exec: 'dom',
+			env: ['lib/mask.js'],
+			tests: 'test/**-dom.test'
+		},
+
+		'mask.html': {
+			exec: 'dom',
+			env: ['lib/mask.node.js'],
+			tests: 'test/**-html.test'
+		}
+	}
+};
