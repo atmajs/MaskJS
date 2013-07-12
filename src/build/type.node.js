808c7904b4903e65a776a756d5c5a3d7071ee361
Alexander Kit <source.kit@gmail.com>
2013-07-12 10:42:07 +0200

    [++] append node before setting attributes

--- src/build/type.node.js
+++ src/build/type.node.js
@@ -21,6 +21,12 @@ if (childs != null){
 	attr['x-compo-id'] = controller.ID;
 }
 
+// ++ insert tag into container before setting attributes, so that in any
+// custom util parentNode is available. This is for mask.node important
+// http://jsperf.com/setattribute-before-after-dom-insertion/2
+if (container != null) {
+	container.appendChild(tag);
+}
 
 
 for (key in attr) {
@@ -52,8 +58,5 @@ for (key in attr) {
 
 }
 
-if (container != null) {
-	container.appendChild(tag);
-}
 
 container = tag;
