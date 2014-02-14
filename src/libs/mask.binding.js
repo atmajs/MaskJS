f7d0f11c98cd0f7158bd0c18dcc38274c73f0251
Alex Kit <source.kit@gmail.com>
2014-02-14 16:28:23 +0100

    [++] update binding

--- src/libs/mask.binding.js
+++ src/libs/mask.binding.js
@@ -713,189 +713,205 @@
 	
 	// end:source ../src/util/compo.js
 	// source ../src/util/expression.js
-	var Expression = mask.Utils.Expression,
-		expression_eval_origin = Expression.eval,
+	var expression_eval,
+		expression_bind,
+		expression_unbind,
+		expression_createBinder,
+		
+		expression_parse,
+		expression_varRefs
+		;
+		
+	(function(){
+		var Expression = mask.Utils.Expression,
+			expression_eval_origin = Expression.eval
+			;
+	
+		expression_parse = Expression.parse;
+		
+		expression_varRefs = Expression.varRefs;
+		
 		expression_eval = function(expr, model, cntx, controller){
-			
-			if (expr === '.') {
+				
+			if (expr === '.') 
 				return model;
-			}
 			
 			var value = expression_eval_origin(expr, model, cntx, controller);
-	
-			return value == null ? '' : value;
-		},
-		expression_parse = Expression.parse,
-		expression_varRefs = Expression.varRefs;
-	
-	
-	function expression_bind(expr, model, cntx, controller, callback) {
-		
-		if (expr === '.') {
+			return value == null
+				? ''
+				: value
+				;
+		};
 			
-			if (arr_isArray(model)) {
-				arr_addObserver(model, callback);
-			}
+		expression_bind = function(expr, model, cntx, controller, callback) {
 			
-			return;
-		}
-		
-		var ast = expression_parse(expr),
-			vars = expression_varRefs(ast),
-			obj, ref;
-	
-		if (vars == null) {
-			return;
-		}
-	
-		if (typeof vars === 'string') {
-			
-			if (obj_isDefined(model, vars)) {
-				obj = model;
-			}
-			
-			if (obj == null && obj_isDefined(controller, vars)) {
-				obj = controller;
-			}
-			
-			if (obj == null) {
-				obj = model;
-			}
-			
-			obj_addObserver(obj, vars, callback);
-			return;
-		}
-	
-		var isArray = vars.length != null && typeof vars.splice === 'function',
-			imax = isArray === true ? vars.length : 1,
-			i = 0,
-			x;
-		
-		for (; i < imax; i++) {
-			x = isArray ? vars[i] : vars;
-			if (x == null) {
-				continue;
-			}
-			
-			
-			if (typeof x === 'object') {
+			if (expr === '.') {
 				
-				obj = expression_eval_origin(x.accessor, model, cntx, controller);
+				if (arr_isArray(model)) 
+					arr_addObserver(model, callback);
 				
-				if (obj == null || typeof obj !== 'object') {
-					console.error('Binding failed to an object over accessor', x);
-					continue;
-				}
-				
-				x = x.ref;
-			} else if (obj_isDefined(model, x)) {
-				
-				obj = model;
-			} else if (obj_isDefined(controller, x)) {
-				
-				obj = controller;
-			} else {
-				
-				obj = model;
-			}
-			
-			obj_addObserver(obj, x, callback);
-		}
-	
-		return;
-	}
-	
-	function expression_unbind(expr, model, controller, callback) {
-		
-		if (typeof controller === 'function') {
-			console.warn('[mask.binding] - expression unbind(expr, model, controller, callback)');
-		}
-		
-		if (expr === '.') {
-			arr_removeObserver(model, callback);
-			return;
-		}
-		
-		var vars = expression_varRefs(expr),
-			x, ref;
-	
-		if (vars == null) {
-			return;
-		}
-		
-		if (typeof vars === 'string') {
-			if (obj_isDefined(model, vars)) {
-				obj_removeObserver(model, vars, callback);
-			}
-			
-			if (obj_isDefined(controller, vars)) {
-				obj_removeObserver(controller, vars, callback);
-			}
-			
-			return;
-		}
-		
-		var isArray = vars.length != null && typeof vars.splice === 'function',
-			imax = isArray === true ? vars.length : 1,
-			i = 0,
-			x;
-		
-		for (; i < imax; i++) {
-			x = isArray ? vars[i] : vars;
-			if (x == null) {
-				continue;
-			}
-			
-			
-			if (typeof x === 'object') {
-				
-				var obj = expression_eval_origin(x.accessor, model, null, controller);
-				
-				if (obj) {
-					obj_removeObserver(obj, x.ref, callback);
-				}
-				
-				continue;
-			}
-			
-			if (obj_isDefined(model, x)) {
-				obj_removeObserver(model, x, callback);
-			}
-			
-			if (obj_isDefined(controller, x)) {
-				obj_removeObserver(controller, x, callback);
-			}
-		}
-	
-	}
-	
-	/**
-	 * expression_bind only fires callback, if some of refs were changed,
-	 * but doesnt supply new expression value
-	 **/
-	function expression_createBinder(expr, model, cntx, controller, callback) {
-		var lockes = 0;
-		return function binder() {
-			if (lockes++ > 10) {
-				console.warn('Concurent binder detected', expr);
 				return;
 			}
 			
-			var value = expression_eval(expr, model, cntx, controller);
-			if (arguments.length > 1) {
-				var args = __array_slice.call(arguments);
+			var ast = expression_parse(expr),
+				vars = expression_varRefs(ast),
+				obj, ref;
+		
+			if (vars == null) 
+				return;
+			
+			if (typeof vars === 'string') {
 				
-				args[0] = value;
-				callback.apply(this, args);
+				if (obj_isDefined(model, vars)) {
+					obj = model;
+				}
 				
-			}else{
+				if (obj == null && obj_isDefined(controller, vars)) {
+					obj = controller;
+				}
 				
-				callback(value);
+				if (obj == null) {
+					obj = model;
+				}
+				
+				obj_addObserver(obj, vars, callback);
+				return;
+			}
+		
+			var isArray = vars.length != null && typeof vars.splice === 'function',
+				imax = isArray === true ? vars.length : 1,
+				i = 0,
+				x;
+			
+			for (; i < imax; i++) {
+				x = isArray
+					? vars[i]
+					: vars;
+				if (x == null) 
+					continue;
+				
+				
+				if (typeof x === 'object') {
+					
+					obj = expression_eval_origin(x.accessor, model, cntx, controller);
+					
+					if (obj == null || typeof obj !== 'object') {
+						console.error('Binding failed to an object over accessor', x);
+						continue;
+					}
+					
+					x = x.ref;
+				}
+				
+				else if (obj_isDefined(model, x)) {
+					obj = model;
+				}
+				
+				else if (obj_isDefined(controller, x)) {
+					obj = controller;
+				}
+				
+				else {
+					obj = model;
+				}
+				
+				
+				if (x == null || x === '$c') 
+					continue;
+				
+				obj_addObserver(obj, x, callback);
+			}
+		
+			return;
+		};
+		
+		expression_unbind = function(expr, model, controller, callback) {
+			
+			if (typeof controller === 'function') 
+				console.warn('[mask.binding] - expression unbind(expr, model, controller, callback)');
+			
+			
+			if (expr === '.') {
+				arr_removeObserver(model, callback);
+				return;
 			}
 			
-			lockes--;
+			var vars = expression_varRefs(expr),
+				x, ref;
+		
+			if (vars == null) 
+				return;
+			
+			if (typeof vars === 'string') {
+				if (obj_isDefined(model, vars)) 
+					obj_removeObserver(model, vars, callback);
+				
+				
+				if (obj_isDefined(controller, vars)) 
+					obj_removeObserver(controller, vars, callback);
+				
+				return;
+			}
+			
+			var isArray = vars.length != null && typeof vars.splice === 'function',
+				imax = isArray === true ? vars.length : 1,
+				i = 0,
+				x;
+			
+			for (; i < imax; i++) {
+				x = isArray
+					? vars[i]
+					: vars;
+				if (x == null) 
+					continue;
+				
+				if (typeof x === 'object') {
+					
+					var obj = expression_eval_origin(x.accessor, model, null, controller);
+					if (obj) 
+						obj_removeObserver(obj, x.ref, callback);
+					
+					continue;
+				}
+				
+				if (obj_isDefined(model, x)) 
+					obj_removeObserver(model, x, callback);
+				
+				if (obj_isDefined(controller, x)) 
+					obj_removeObserver(controller, x, callback);
+			}
+		
+		}
+		
+		/**
+		 * expression_bind only fires callback, if some of refs were changed,
+		 * but doesnt supply new expression value
+		 **/
+		expression_createBinder = function(expr, model, cntx, controller, callback) {
+			var locks = 0;
+			return function binder() {
+				if (locks++ > 10) {
+					console.warn('Concurent binder detected', expr);
+					return;
+				}
+				
+				var value = expression_eval(expr, model, cntx, controller);
+				if (arguments.length > 1) {
+					var args = __array_slice.call(arguments);
+					
+					args[0] = value;
+					callback.apply(this, args);
+					
+				} else {
+					
+					callback(value);
+				}
+				
+				locks--;
+			};
 		};
-	}
+		
+	}());
 	
 	
 	
