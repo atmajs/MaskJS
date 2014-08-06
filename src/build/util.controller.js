032a8b3badf1e38cf8d07dab9486ba97c686773a
Alex Kit <source.kit@gmail.com>
2014-08-06 03:54:08 +0200

    v0.9.3

--- src/build/util.controller.js
+++
@@ -1,9 +0,0 @@
-function controller_pushCompo(controller, compo) {
-	
-	if (controller.components == null) {
-		controller.components = [ compo ];
-		return;
-	} 
-	
-	controller.components.push(compo);
-}
