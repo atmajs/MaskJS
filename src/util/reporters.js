48f41b294e1d64a848564bfc6254ff872519989b
Alex Kit <source.kit@gmail.com>
2014-11-07 16:53:22 +0100

    * Better parser error visualization

--- src/util/reporters.js
+++ src/util/reporters.js
@@ -1,6 +1,7 @@
 var throw_,
 	parser_error,
 	parser_warn,
+	log,
 	log_warn,
 	log_error;
 	
@@ -22,15 +23,16 @@ var throw_,
 	parser_warn = function(msg, str, i, token, state, file){
 		var error = createMsg('warn', msg, str, i, token, state, file);
 		log_warn(error.message);
-		log_warn(error.stack);
+		log(error.stack);
 		listeners_emit('error', error);
 	};
 	
 	if (typeof console === 'undefined') {
-		log_warn = log_error = function(){};
+		log = log_warn = log_error = function(){};
 	}
 	else {
 		var bind  = Function.prototype.bind;
+		log       = bind.call(console.warn , console);
 		log_warn  = bind.call(console.warn , console, 'MaskJS [Warn] :');
 		log_error = bind.call(console.error, console, 'MaskJS [Error] :');
 	}
@@ -67,8 +69,9 @@ var throw_,
 	function createMsg(type, msg, str, index, token, state, filename){
 		msg += formatToken(token)
 			+ formatFilename(str, index, filename)
-			+ formatStopped(type, str, index)
+			+ '\nParser '
 			+ formatState(state)
+			+ formatStopped(type, str, index)
 			;
 		
 		var Ctor = type === 'error'
@@ -87,13 +90,12 @@ var throw_,
 		return ' Invalid token: `'+ token + '`';
 	}
 	function formatFilename(str, index, filename) {
-		if (index == null && !filename) 
+		if (index == null || !filename) 
 			return '';
 		
-		var lines = str.substring(0, index).split('\n'),
-			line = lines.length,
-			row = index + 1 - lines.slice(0, line - 2).join('\n').length;
-		
+		var lines = splitLines(str, index),
+			line = lines[1],
+			row  = lines[2];
 		return ' at '
 			+ (filename || '')
 			+ '(' + line + ':' + row + ')';
@@ -111,16 +113,78 @@ var throw_,
 		if (state == null || states[state] == null) 
 			return '';
 		
-		return '\n    , when parsing ' + states[state];
+		return ' on "' + states[state] + '"';
 	}
 	function formatStopped(type, str, index){
 		if (index == null) 
 			return '';
 		
-		var stopped = str.substring(index);
-		if (stopped.length > 30) 
-			stopped = stopped.substring(0, 30) + '...';
+		var data = splitLines(str, index),
+			lines = data[0],
+			line  = data[1],
+			row   = data[2];
+			
+		return index == null
+			? ''
+			: ' at ('
+				+ line
+				+ ':'
+				+ row
+				+ ') \n'
+				+ formatCursor(lines, line, row)
+			;
+	}
+	
+	var formatCursor;
+	(function(){
+		formatCursor = function(lines, line, row) {
+			var BEFORE = 3,
+				AFTER  = 2,
+				i = (line - 1) - BEFORE,
+				imax   = i + BEFORE + AFTER,
+				str  = '';
+			
+			if (i < 0) i = 0;
+			if (imax > lines.length) imax = lines.length;
+			
+			var lineNumberLength = String(imax).length,
+				lineNumber;
+			
+			for(; i < imax; i++) {
+				if (str)  str += '\n';
+				
+				lineNumber = ensureLength(i + 1, lineNumberLength);
+				str += lineNumber + '|' + lines[i];
+				
+				if (i + 1 === line) {
+					str += '\n' + repeat(' ', lineNumberLength + 1);
+					str += lines[i].substring(0, row - 2).replace(/[^\s]/g, ' ');
+					str += '^';
+				}
+			}
+			return str;
+		};
 		
-		return '\n    Parser ' + type + ' at: ' + stopped;
+		function ensureLength(num, count) {
+			var str = String(num);
+			while(str.length < count) {
+				str += ' ';
+			}
+			return str;
+		}
+		function repeat(char_, count) {
+			var str = '';
+			while(--count > -1) {
+				str += char_;
+			}
+			return str;
+		}
+	}());
+	
+	function splitLines(str, index) {
+		var lines = str.substring(0, index).split('\n'),
+			line = lines.length,
+			row = index + 1 - lines.slice(0, line - 2).join('\n').length;
+		return [str.split('\n'), line, row];
 	}
 }());
