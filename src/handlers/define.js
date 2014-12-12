3043644b12d443918c6af227d000ca0b719327ce
Alex Kit <source.kit@gmail.com>
2014-12-12 11:35:54 +0100

    * `Define` supports function declaration

--- src/handlers/define.js
+++ src/handlers/define.js
@@ -16,10 +16,23 @@
 		for(name in this.attr) break;
 		
 		var nodes = this.nodes;
-		mask.registerHandler(name, Compo({
+		var Proto = {
 			renderStart: function(){
 				this.nodes = mask.merge(nodes, this.nodes || [], this);
+				if (this.onRenderStart != null) 
+					this.onRenderStart.apply(this, arguments);
 			}
-		}));
+		};
+		var imax = nodes.length,
+			i = 0, x;
+		for(; i<imax; i++) {
+			x = nodes[i];
+			if (x.tagName === 'function') {
+				Proto[x.name] = x.fn;
+				x.render = null;
+			}
+		}
+		mask.registerHandler(name, Compo(Proto));
 	}
+	
 }(Mask));
