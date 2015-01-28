398a7f866c0e50e85582d0c5634a250837721ed7
Alex Kit <source.kit@gmail.com>
2015-01-28 17:13:23 +0100

    + log file load error

--- src/util/resource/transports/xhr.js
+++ src/util/resource/transports/xhr.js
@@ -3,16 +3,18 @@ var xhr_get;
 	xhr_get = function(path, cb){
 		var xhr = new XMLHttpRequest();
 		xhr.onreadystatechange = function() {
-			if (xhr.readyState === 4){
-				var res = xhr.responseText, err;
-				if (xhr.status !== 200) {
-					err = {
-						status: xhr.status,
-						content: res
-					};
-				}
-				cb(err, res);
+			if (xhr.readyState !== 4)
+				return;
+			
+			var res = xhr.responseText, err;
+			if (xhr.status !== 200) {
+				err = {
+					status: xhr.status,
+					content: res
+				};
+				log_warn('File error', path, xhr.status);
 			}
+			cb(err, res);
 		};
 
 		xhr.open('GET', path, true);
