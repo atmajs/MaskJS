258f10960b5d433595b7093317d4e7804387b8db
Alex Kit <source.kit@gmail.com>
2015-02-16 12:17:11 +0100

    + `repeat` statement

---
+++ src/statements/10.repeat.js
@@ -0,0 +1,50 @@
+(function(){
+	custom_Statements['repeat'] = {
+		render: function(node, model, ctx, container, ctr, children){
+			var run = ExpressionUtil.eval,
+				str = node.expression,
+				repeat = str.split('..'),
+				index = + run(repeat[0] || '', model, ctx, ctr),
+				length = + run(repeat[1] || '', model, ctx, ctr);
+
+			if (index !== index || length !== length) {
+				log_error('Repeat attribute(from..to) invalid', str);
+				return;
+			}
+
+			var nodes = node.nodes;
+			var arr = [];
+			var i = -1;
+			while (++i < length) {
+				arr[i] = compo_init(
+					'repeat::item',
+					nodes,
+					model,
+					i,
+					container,
+					ctr
+				);
+			}
+
+			var els = [];
+			builder_build(arr, model, ctx, container, ctr, els);
+			arr_pushMany(children, els);
+		}
+	};
+
+	function compo_init(name, nodes, model, index, container, parent) {		
+		return {
+			type: Dom.COMPONENT,
+			compoName: name,
+			attr: {},
+			nodes: nodes,
+			model: model,
+			container: container,
+			parent: parent,
+			index: index,
+			scope: {
+				index: index
+			}
+		};
+	}
+}());
