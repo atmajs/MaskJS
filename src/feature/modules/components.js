36813411334a6e41f0518cdf251140db5d4bd6ea
Alex Kit <source.kit@gmail.com>
2015-03-20 16:50:07 +0100

    v0.12.11

--- src/feature/modules/components.js
+++
@@ -1,7 +0,0 @@
-custom_Tags['module'] = class_create({
-	constructor: function(node, model, ctx, container, ctr) {
-		var path  = path_resolveUrl(node.attr.path, u_resolveLocation(ctx, ctr));
-		Module.registerModule(node.nodes, path, ctx, ctr);
-	},
-	render: fn_doNothing
-});
