85be56b4f8a4d63a059db6115db0d7bb56f1619b
Alex Kit <source.kit@gmail.com>
2015-06-10 00:16:30 +0200

    add (method handlers) append source url for better debugging

--- src/parser/parsers/methods.js
+++ src/parser/parsers/methods.js
@@ -31,11 +31,14 @@
 		this.name = name;
 		this.args = args;
 	}
-	function compileFn(args, body) {
+	function compileFn(args, body, sourceUrl) {
 		var arr = _Array_slice.call(args);
 		var compile = __cfg.preprocessor.script;
 		if (compile != null) {
 			body = compile(body);
+		}
+		if (sourceUrl != null) {
+			body += '\n//# sourceURL=' + sourceUrl
 		}
 		arr.push(body);			
 		return new (Function.bind.apply(Function, [null].concat(arr)));
@@ -54,7 +57,16 @@
 			this.args = args;
 			this.body = body;
 			this.parent = parent;
-			this.fn = compileFn(args, body);
+			
+			var sourceUrl = null;
+			//if DEBUG
+			var ownerName = parent.tagName;
+			if (ownerName === 'let' || ownerName === 'define') {
+				ownerName += '_' + parent.name;
+			}
+			sourceUrl = constructSourceUrl(tagName, name, parent);
+			//endif
+			this.fn = compileFn(args, body, sourceUrl);
 		},
 		stringify: function(stream){
 			var head = this.tagName
@@ -69,6 +81,43 @@
 			stream.closeBlock('}');
 		}
 	});
+	
+	var constructSourceUrl;
+	(function(){
+		constructSourceUrl = function (methodType, methodName, owner) {
+			var ownerName = owner.tagName,
+				parent = owner,
+				stack = '',
+				tag;
+			while(parent != null) {
+				tag = parent.tagName;
+				if ('let' === tag || 'define' === tag) {
+					if (stack !== '') {
+						stack = '.' + stack;
+					}
+					stack = parent.name + stack;
+				}
+				parent = parent.parent;
+			}
+			if ('let' !== ownerName && 'define' !== ownerName) {
+				if (stack !== '') {
+					stack += '_';
+				}
+				stack += ownerName
+			}
+			var url = stack + '_' + methodType + '_' + methodName;
+			var index = null
+			if (_sourceUrls[url] !== void 0) {
+				index = ++_sourceUrls[url];
+			}
+			if (index != null) {
+				url += '_' + index;
+			}
+			_sourceUrls[url] = 1;
+			return 'dynamic://MaskJS/' + url;
+		};
+		var _sourceUrls = {};
+	}());
 	
 	custom_Parsers['slot' ]    = create('slot');
 	custom_Parsers['event']    = create('event');
