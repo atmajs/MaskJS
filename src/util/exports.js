8806c3042c790808dfa9a550afba8e9282df9cd4
Alex Kit <source.kit@gmail.com>
2015-07-14 23:47:08 +0200

    v0.52.1

--- src/util/exports.js
+++
@@ -1,8 +0,0 @@
-// import ./util.js
-// import ./attr.js
-// import ./array.js
-// import ./object.js
-// import ./listeners.js
-// import ./reporters.js
-// import ./path.js
-// import ./resource/file.js
