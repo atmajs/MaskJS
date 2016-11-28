e945ff1a207324a18cd9ee329b66cdfd8da5eebc
Alex Kit <source.kit@gmail.com>
2016-11-28 15:07:59 +0100

    fix (perf) use multiple arg values instead of `arguments` meterialization

--- src/util/listeners.js
+++ src/util/listeners.js
@@ -31,26 +31,17 @@ var listeners_on,
 		}
 		arr_remove(bin[event], fn);
 	};
-	listeners_emit = function(event){
+	listeners_emit = function(event, v1, v2, v3, v4){
 		var fns = bin[event];
 		if (fns == null) {
 			return false;
 		}
 		var imax = fns.length,
 			i = -1;
-		if (imax === 0) {
-			return false;
-		}
-		var j = 0,
-			jmax = arguments.length,
-			args = new Array(jmax - 1);
-		while(++j < jmax) {
-			args[j-1] = arguments[j];
-		}
 		while ( ++i < imax) {
-			fns[i].apply(null, args);
+			fns[i](v1, v2, v3, v4);
 		}
-		return true;
+		return i !== 0;
 	};
 
 	// === private
