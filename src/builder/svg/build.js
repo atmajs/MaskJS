ed2aada7d4b0eea6eaa5a688c240679aa666d658
Alex Kit <source.kit@gmail.com>
2016-12-12 13:26:29 +0100

    v0.59.3

--- src/builder/svg/build.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	builder_buildSVG = builder_buildDelegate({
-		create: function(name, doc){
-			return doc.createElementNS(SVG_NS, name);
-		}
-	});
-	
-	var SVG_NS = 'http://www.w3.org/2000/svg';
-}());
