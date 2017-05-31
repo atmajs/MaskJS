0b006ecdaafb5bbc45a5602225093c45041c4323
Alex Kit <source.kit@gmail.com>
2017-05-31 08:55:11 +0200

    v0.64.1

--- typings/utils.d.ts
+++
@@ -1,8 +0,0 @@
-declare module mask {
-
-	class ObjUtils {
-		get: (mix: any, path: string) => any
-		set: (mix: any, path: string, val: any) => void
-		extend<T1, T2> (a: T1, b: T2): T1 & T2
-	}
-}
