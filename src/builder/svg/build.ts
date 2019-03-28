3afd65b6a649c730234f537a17b0b7f906bddf6f
Alex Kit <source.kit@gmail.com>
2019-03-28 12:05:30 +0100

    v0.7.2

--- src/builder/svg/build.ts
+++
@@ -1,9 +0,0 @@
-import { builder_buildDelegate } from '../delegate/exports';
-
-export const builder_buildSVG = builder_buildDelegate({
-    create: function(name, doc){
-        return doc.createElementNS(SVG_NS, name);
-    }
-});
-
-var SVG_NS = 'http://www.w3.org/2000/svg';
