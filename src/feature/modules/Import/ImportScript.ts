3167739525164f1fd7ca2a2ca6662952d7a578a5
Alex Kit <source.kit@gmail.com>
2019-06-04 02:13:39 +0200

    v0.7.6

--- src/feature/modules/Import/ImportScript.ts
+++
@@ -1,8 +0,0 @@
-import { IImport } from './Import';
-import { i_Types } from './ImportTypes';
-import { class_create } from '@utils/class';
-
-export const ImportScript = i_Types['script'] = class_create(IImport, {
-	type: 'script',
-	contentType: 'script'	
-});
