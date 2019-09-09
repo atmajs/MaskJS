b66996de054be0292ee75406027d362da8fa50b1
Alex Kit <source.kit@gmail.com>
2019-09-09 12:32:48 +0200

    v0.7.9

--- projects/mask-binding/src/binders/ExpressionBinder.ts
+++
@@ -1,8 +0,0 @@
-import { IBinder } from './IBinder';
-import { expression_bind, expression_unbind } from '@project/observer/src/exports';
-import { class_create } from '@utils/class';
-
-export const ExpressionBinder = class_create(IBinder, {
-	on: expression_bind,
-	off: expression_unbind
-});
--- src/builder/delegate/IBuilderConfig.ts
+++
@@ -1,3 +0,0 @@
-export interface IBuilderConfig {
-    create: (name: string, doc: any) => any;
-}
