a44be903d896b599d48f1bc4b38c7c03253e346a
Alex Kit <source.kit@gmail.com>
2019-09-13 13:51:46 +0200

    v0.7.13

--- tools/build-dts.js
+++
@@ -1,9 +0,0 @@
-var dts = require('dts-bundle');
-
-dts.bundle({
-	name: 'mask',
-	main: './ts-temp/src/mask.d.ts',
-	out: './out/index.d.ts'
-});
-
-io.File.copyTo('./ts-temp/src/out/index.d.ts', './lib/mask.d.ts');
