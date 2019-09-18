4a64d272efcaa472112e29047328209e9892619e
Alex Kit <source.kit@gmail.com>
2019-09-18 23:02:56 +0200

    v0.7.17

--- src/feature/modules/Module/ModuleStyle.ts
+++
@@ -1,9 +0,0 @@
-import { IModule } from './Module';
-import { m_Types } from './ModuleTypes';
-import { class_create } from '@utils/class';
-import { _file_getStyle } from '../loaders';
-
-export const ModuleStyle = m_Types['style'] = class_create(IModule, {
-	type: 'style',
-	load_: _file_getStyle
-});
