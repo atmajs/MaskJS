489f9f108d979676d28ecf3aa7391a5e2068e5ad
Alex Kit <source.kit@gmail.com>
2019-10-07 01:03:40 +0200

    v0.7.23

--- src/builder/delegate/exports.ts
+++
@@ -1,6 +0,0 @@
-import { builder_buildFactory } from './build';
-import { IBuilderConfig } from './IBuilderConfig';
-
-export function builder_buildDelegate (opts: IBuilderConfig){		
-    return builder_buildFactory(opts);
-};
