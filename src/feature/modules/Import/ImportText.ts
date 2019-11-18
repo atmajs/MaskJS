4228a6a44b40e2b1b663e263508b50054b486a9a
Alex Kit <source.kit@gmail.com>
2019-11-19 00:32:47 +0100

    v0.7.31

--- src/feature/modules/Import/ImportText.ts
+++
@@ -1,8 +0,0 @@
-import { i_Types } from './ImportTypes';
-import { class_create } from '@utils/class';
-import { ImportScript } from './ImportScript';
-
-export const ImportText = i_Types['text'] = class_create(ImportScript, {
-	type: 'text',
-	contentType: 'txt'
-});
