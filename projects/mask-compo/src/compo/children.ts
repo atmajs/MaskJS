731e14ab4d3a64353ed9a72d7f02915fa3b3ba01
Alex Kit <source.kit@gmail.com>
2020-02-17 00:42:12 +0100

    fix (Compo - children selector) use filter flow also for native query selectors

--- projects/mask-compo/src/compo/children.ts
+++ projects/mask-compo/src/compo/children.ts
@@ -4,65 +4,78 @@ import { CompoConfig } from './CompoConfig';
 
 export const Children_ = {
 
-	/**
-	 *	Component children. Example:
-	 *
-	 *	Class({
-	 *		Base: Compo,
-	 *		Construct: function(){
-	 *			this.compos = {
-	 *				panel: '$: .container',  // querying with DOMLib
-	 *				timePicker: 'compo: timePicker', // querying with Compo selector
-	 *				button: '#button' // querying with querySelector***
-	 *			}
-	 *		}
-	 *	});
-	 *
-	 */
-	select (component, compos) {
-		for (var name in compos) {
-			var data = compos[name],
-				events = null,
-				selector = null;
+    /**
+    *	Component children. Example:
+    *
+    *	Class({
+    *		Base: Compo,
+    *		Construct: function(){
+    *			this.compos = {
+    *				panel: '$: .container',  // querying with DOMLib
+    *				timePicker: 'compo: timePicker', // querying with Compo selector
+    *				button: '#button' // querying with querySelector***
+    *			}
+    *		}
+    *	});
+    *
+    */
+    select(component, compos) {
+        for (var name in compos) {
+            var data = compos[name],
+                events = null,
+                selector = null;
 
-			if (data instanceof Array) {
+            if (data instanceof Array) {
                 console.error('obsolete');
-				selector = data[0];
-				events = data.splice(1);
-			}
-			if (typeof data === 'string') {
-				selector = data;
-			}
-			if (data == null || selector == null) {
-				log_error('Unknown component child', name, compos[name]);
-				log_warn('Is this object shared within multiple compo classes? Define it in constructor!');
-				return;
-			}
+                selector = data[0];
+                events = data.splice(1);
+            }
+            if (typeof data === 'string') {
+                selector = data;
+            }
+            if (data == null || selector == null) {
+                log_error('Unknown component child', name, compos[name]);
+                log_warn('Is this object shared within multiple compo classes? Define it in constructor!');
+                return;
+            }
 
-			var index = selector.indexOf(':'),
-				engine = selector.substring(0, index);
+            let index = selector.indexOf(':');
+            let engine = CompoConfig.selectors [
+                selector.substring(0, index)
+            ];
 
-			engine = CompoConfig.selectors[engine];
+            if (engine == null) {
+                let $els: HTMLElement[] = component.$;
+                let el: HTMLElement;
+                for (let i = 0; i < $els.length; i++) {
+                    let x = $els[i];
+                    el = x.querySelector(selector);
+                    if (el != null) {
+                        break;
+                    }
+                    if (x.matches(selector)) {
+                        el = x;
+                        break;
+                    }
+                }
+                component.compos[name] = el;
+            } else {
+                selector = selector.substring(++index).trim();
+                component.compos[name] = engine(component, selector);
+            }
 
-			if (engine == null) {
-				component.compos[name] = component.$[0].querySelector(selector);
-			} else {
-				selector = selector.substring(++index).trim();
-				component.compos[name] = engine(component, selector);
-			}
+            var element = component.compos[name];
 
-			var element = component.compos[name];
+            if (events != null) {
+                if (element.$ != null) {
+                    element = element.$;
+                }
 
-			if (events != null) {
-				if (element.$ != null) {
-					element = element.$;
-				}
-				
-				Events_.on(component, events, element);
-			}
-		}
+                Events_.on(component, events, element);
+            }
+        }
     },
-    selectSelf (self, refs: { compos?: any, elements?: any, queries?: any } ) {
+    selectSelf(self, refs: { compos?: any, elements?: any, queries?: any }) {
         let compos = refs.compos;
         if (compos) {
             for (let prop in compos) {
