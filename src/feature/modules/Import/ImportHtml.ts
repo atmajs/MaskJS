5b356352513ced0f3a5777620841a530fe2eb686
Alex Kit <source.kit@gmail.com>
2020-03-29 15:06:43 +0200

    v0.71.65

--- src/feature/modules/Import/ImportHtml.ts
+++
@@ -1,8 +0,0 @@
-import { i_Types } from './ImportTypes';
-import { class_create } from '@utils/class';
-import { ImportMask } from './ImportMask';
-
-export const ImportHtml = i_Types['html'] = class_create(ImportMask, {
-	type: 'mask',
-	contentType: 'html'
-});
