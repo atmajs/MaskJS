81483aedbff137cbce22c2da7baedb5a0d9fa5d8
aki <alex.kit@atmajs.com>
2020-06-29 10:06:49 +0200

    v0.71.93

--- projects/mask-binding/src/utils/expression.ts
+++
@@ -1,6 +0,0 @@
-import { expression_eval } from '@project/expression/src/exports'
-
-export function expression_eval_safe (expr, model, ctx, ctr, node?){
-    const x = expression_eval(expr, model, ctx, ctr, node);
-    return x == null ? '' : x;
-};
