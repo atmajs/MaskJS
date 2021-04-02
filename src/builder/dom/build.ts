3f3d7a8852409d63f5fc95e6f7a0175d14349d0f
Alex Kit <alex.kit@atmajs.com>
2021-04-02 18:47:27 +0200

    v0.72.13

--- src/builder/dom/build.ts
+++
@@ -1,7 +0,0 @@
-import { builder_buildDelegate } from '../delegate/exports';
-
-export const builder_build = builder_buildDelegate({
-    create (name, doc) {
-        return doc.createElement(name);
-    }
-});
--- src/feature/modules/Import/ImportData.ts
+++
@@ -1,8 +0,0 @@
-import { i_Types } from './ImportTypes';
-import { class_create } from '@utils/class';
-import { ImportScript } from './ImportScript';
-
-export const ImportData = i_Types['data'] = class_create(ImportScript, {
-	type: 'data',
-	contentType: 'json'
-});
