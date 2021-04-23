84cd7107e938004505ef4bed731be0ab38121b4a
Alex Kit <alex.kit@atmajs.com>
2021-04-23 17:38:02 +0200

    v0.72.17

--- src/autoreload/wrapper.js
+++
@@ -1,6 +0,0 @@
-(function(mask) {
-    'use strict';
-    
-    /**MODULE**/
-
-}(mask));
--- src/builder/BuilderData.ts
+++
@@ -1,4 +0,0 @@
-export const BuilderData = {
-    id: 1,
-    document: typeof document === 'undefined' ? null : document
-};
