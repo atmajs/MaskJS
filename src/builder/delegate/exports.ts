9aec85e2e7de7e9ed2b834f36afeb7af869c2826
Alex Kit <alex.kit@atmajs.com>
2021-04-28 11:56:34 +0200

    v0.72.23

--- src/builder/delegate/exports.ts
+++
@@ -1,6 +0,0 @@
-import { builder_buildFactory } from './builder_buildFactory';
-import { IBuilderConfig } from './IBuilderConfig';
-
-export function builder_buildDelegate (opts: IBuilderConfig){		
-    return builder_buildFactory(opts);
-};
