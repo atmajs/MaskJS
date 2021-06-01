c4590c992a641d91e409f55154b39ee0261aa9bd
Alex Kit <alex.kit@atmajs.com>
2021-06-01 12:15:02 +0200

    v0.72.27

--- projects/mask-binding/src/utils/exports.ts
+++
@@ -1,7 +0,0 @@
-// import object
-// import object_observe
-// import date
-// import dom
-// import compo
-// import expression
-// import signal
--- src/builder/delegate/IBuilderConfig.ts
+++
@@ -1,5 +0,0 @@
-export interface IBuilderConfig {
-    document?: Document
-    create: (name: string, doc: any) => any;
-    //build_compoFactory?: (build: Function, config: IBuilderConfig) => (node, model_, ctx, container_, ctr_, children_?) => any
-}
