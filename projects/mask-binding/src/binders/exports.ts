ced7529f933befec8f812180effebcc5d6f37b0d
Alex Kit <alex.kit@atmajs.com>
2021-07-17 15:36:08 +0200

    v0.72.35

--- projects/mask-binding/src/binders/exports.ts
+++
@@ -1,9 +0,0 @@
-import { EventEmitterBinder } from './EventEmitterBinder';
-import { ExpressionBinder } from './ExpressionBinder';
-import { RxBinder } from './RxBinder';
-
-export var Binders = {
-    EventEmitterBinder: EventEmitterBinder,
-    ExpressionBinder: ExpressionBinder,
-    RxBinder: RxBinder
-};
